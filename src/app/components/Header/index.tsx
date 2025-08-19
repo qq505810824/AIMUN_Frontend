d500ace2c535ad5183e3542da475fde7fe3dfed4
聪 <505810824@qq.com>
2025-08-19 18:02:47 +0800

    update head

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -107,9 +107,9 @@ export default function HeaderView(props: ViewProps) {
 
                         </div>
                         <nav className="hidden md:flex items-center gap-1 text-sm">
-                            {links.map((link) => {
+                            {links.map((link, index) => {
                                 return (
-                                    <Link className="px-3 py-2 rounded-xl hover:text-blue-600 hover:bg-gray-100" href={link.href}>{link.label}</Link>
+                                    <Link key={index} className="px-3 py-2 rounded-xl hover:text-blue-600 hover:bg-gray-100" href={link.href}>{link.label}</Link>
                                 );
                             })}
                         </nav>
